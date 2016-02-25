c3c0a42d2ecb898948db04562b8325ba6e21c7f7
maanitmehra <maanitm@gmail.com>
2016-02-25 03:44:57 -0500

    Completed Q3 part a

--- hw2/hw2.py
+++ hw2/hw2.py
@@ -2,9 +2,13 @@ import csv
 import numpy as np
 import math
 from collections import Counter
+import matplotlib as mpl
+import matplotlib.pyplot as plt
+import matplotlib.mlab as mlab
 
-MAX_K_VAL=5
 
+MAX_K_VAL=9
+NUM_IMAGES=3
 ## Function to read Data from the file
 def getData(file):
         reader=csv.reader(open(file,"rb"),delimiter=',')
@@ -56,10 +60,29 @@ def knn_main(K_max,X_train, y_train, vec_test):
 		label_arr_sorted=label_arr[label_arr[:,1].argsort()[::-1]]
 		label		=label_arr_sorted[0,0]	
 		y_pred[0,k-1]	=label
-#		print label
 	return y_pred
 
-def Q3_a(X_train,X_test,y_train,y_Act,Q):
+def conMat(y_pred,y_act):
+	# C--> ConMatrix
+	C=np.zeros((10,10))
+	for i in range(0, np.size(y_pred)):
+		real=int(y_act[i])
+		pred=int(y_pred[i])
+		C[real,pred]=C[real,pred]+1
+	return C
+
+def dispIneq(y_pred,y_act, filename, tst_img_arr):
+	ind=[]
+	for i in range(0,len(y_pred)):
+		if y_pred[i] != y_act[i]:
+			ind.append(i) 
+	for i in range(0,NUM_IMAGES):
+		arr=tst_img_arr[:,ind[i]]
+		plt.imshow(arr.reshape((28,28)))
+		filepath="./images/%s_act%d_pred%d_%d.png"%(filename,y_act[ind[i]],y_pred[ind[i]],i+1)
+		plt.savefig(filepath)
+
+def Q3_a(X_train,X_test,y_train,y_Act,tst_img_arr):
 	K_max 		= MAX_K_VAL
 	num_test 	= np.size(X_test,axis=0)#number of test vectors
 	# y_pred stores the final predicted values,
@@ -68,41 +91,33 @@ def Q3_a(X_train,X_test,y_train,y_Act,Q):
 	y_pred		= np.zeros((num_test, K_max))
 	for i in range(num_test):
 		y_pred[i,:]	= knn_main(K_max, X_train, y_train, X_test[i,:])
-	print np.hstack((y_pred,y_Act))
+		if ((i*10)%num_test==0):
+			print "\t",i*100.0/num_test , "% loaded"
+
+#	print np.hstack((y_pred,y_Act))
+	print "\t\tk\t\tPrediction Accuracy"
+	for k in range(0,K_max):
+#		print "\tConfusion Matrix for k=%d:"%(k+1)
+		C = conMat(y_pred[:,k],y_Act)
+		print "\t\t%d\t\t\t%.2f%%"%(k+1,(np.trace(C)*100/500.0))
+		if ((k+1)%2!=0):
+			dispIneq(y_pred[:,k], y_Act, 'k_%d'%(k+1), tst_img_arr)
 
 def main():
 
 	PATH='./support_files/mnist_csv/'
 
-	X_train	=getData(PATH+'Xtrain.txt')
-	y_train	=getData(PATH+'label_train.txt')
-	Q	=getData(PATH+'Q.txt')	
-	X_test	=getData(PATH+'Xtest.txt')
-	y_Act	=getData(PATH+'label_test.txt')
+	X_train		=getData(PATH+'Xtrain.txt')
+	y_train		=getData(PATH+'label_train.txt')
+	Q		=getData(PATH+'Q.txt')	
+	X_test		=getData(PATH+'Xtest.txt')
+	y_Act		=getData(PATH+'label_test.txt')
+	tst_img_arr	=np.dot(Q,np.transpose(X_test))
 
-	print np.size(X_test,axis=0)
-	print np.size(y_Act,axis=0)
 	print "Q3 part a..."
-	Q3_a(X_train,X_test,y_train,y_Act,Q)
-	## y_pred: Predicted Array of Labels
-#	y_pred	=Q3_a(X_train,y_train,X_test,y_Act)	
+	Q3_a(X_train,X_test,y_train,y_Act,tst_img_arr)
+	
+	### DEBUGGING ###
+#	dispIneq(y_Act[::-1],y_Act,"k_1",tst_img_arr)
 
-	#### TESTING IN MAIN FUNCTION
-#	vec2=knn_main(X_train,y_train,X_test[-1,:])
-#	vec=euclidVect(X_train,y_train,X_test[-1,:])
-#	a= vec[vec[:,0].argsort()]
-#	print a
-#	k=2
-#	zz= a[0:k,-1]
-#	print zz
-#	Counter(zz)
-#       labels          =Counter(zz).keys()
-#	label_counts    =Counter(zz).values()
-#      	label_arr       =np.transpose(np.vstack((labels,label_counts)))
-#	print "label_arr:\t",label_arr
-#      	label_arr_sorted=label_arr[label_arr[:,1].argsort()[::-1]]
-#	print "label_arr_after_sorting\t", label_arr_sorted
-#      	label           =label_arr_sorted[0,0]
-#	print label
-#	print "vec from function:", vec2
 main()
